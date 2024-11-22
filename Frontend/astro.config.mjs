5b3ca130cf8e788558e8e63f334fda8370cb0d92
Corro-Kun <networpava@gmail.com>
2024-11-22 11:52:39 -0500

    added node ssr

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -2,11 +2,19 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+
   devToolbar: {
-		enabled: false,
-	},
+        enabled: false,
+    },
+
   output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
